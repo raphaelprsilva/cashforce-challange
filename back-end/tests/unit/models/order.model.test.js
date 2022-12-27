218633f5cb7e8f5256e4ffbea127b14398e6e17c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-27 13:59:10 -0300

    test(model): ensures model has name `Order`

---
+++ back-end/tests/unit/models/order.model.test.js
@@ -0,0 +1,23 @@
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+const OrderModel = require('../../../src/models/order.model');
+
+describe('OrderModel tests ', () => {
+  const Order = OrderModel(sequelize, dataTypes);
+  const order = new Order();
+
+  describe('Has name "Order"', () => {
+    checkModelName(Order)('Order');
+  });
+});
