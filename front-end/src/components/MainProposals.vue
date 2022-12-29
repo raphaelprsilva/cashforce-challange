5744bfcd40791bb764845aad4cac68a6e57c00a1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-29 18:52:06 -0300

    feat(front-end): implements `MainProposals` component

---
+++ front-end/src/components/MainProposals.vue
@@ -0,0 +1,23 @@
+<script>
+import SideBar from "./SideBar.vue";
+
+export default {
+  name: "MainProposals",
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<template>
+  <main class="main-container">
+    <SideBar />
+  </main>
+</template>
+
+<style>
+.main-container {
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
