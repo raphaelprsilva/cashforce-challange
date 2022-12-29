cf8689087e9fa9dffc507c5614a29e2b715363a2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-29 18:49:07 -0300

    feat(front-end): implements `MenuLogo` component

---
+++ front-end/src/components/MenuLogo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menu-hero">
+    <div class="sider-logo">
+      <img class="logo" :src="logo" alt="Logo" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuLogo",
+  props: ["logo"],
+};
+</script>
+
+<style>
+.menu-hero {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+}
+
+.sider-logo {
+  height: 80px;
+  min-width: 200px;
+  position: relative;
+}
+
+.logo {
+  height: 30px;
+  left: 24px;
+  position: absolute;
+  top: 25px;
+  width: 160px;
+}
+</style>
