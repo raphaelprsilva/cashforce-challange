1245635555eee7ecab749595c25f390079fe8f11
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-29 14:50:05 -0300

    feat(front-end): add global styles
    add global, reset and style guide css files

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "./assets/styles/reset.css";
+import "./assets/styles/styleguide.css";
+import "./assets/styles/global.css";
+
 createApp(App).mount("#app");
