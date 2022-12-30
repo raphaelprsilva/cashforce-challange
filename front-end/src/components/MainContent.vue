1d732a1b35f6424f29f984bdfe4c913150ce21ac
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-30 14:41:27 -0300

    feat(front-end): implements `MainContent` component

---
+++ front-end/src/components/MainContent.vue
@@ -0,0 +1,31 @@
+<script>
+import TopHeader from "./TopHeader.vue";
+import PageHeader from "./PageHeader.vue";
+import OrdersTable from "./OrdersTable.vue";
+
+export default {
+  name: "MainContent",
+  components: {
+    TopHeader,
+    PageHeader,
+    OrdersTable,
+  },
+};
+</script>
+
+<template>
+  <section class="main-section-container">
+    <TopHeader />
+    <PageHeader />
+    <OrdersTable />
+  </section>
+</template>
+
+<style>
+.main-section-container {
+  display: flex;
+  padding-left: 20rem;
+  flex-direction: column;
+  min-width: 100%;
+}
+</style>
