cd0a3193eb0fcc5c14ff70f16faca8f742e782ef
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-30 11:44:05 -0300

    feat(front-end): implements `TopHeader` component

---
+++ front-end/src/components/TopHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "TopHeader",
+};
+</script>
+
+<template>
+  <div class="top-header-container"></div>
+</template>
+
+<style>
+.top-header-container {
+  align-items: flex-start;
+  display: flex;
+  height: 64px;
+  border-bottom: 1px solid var(--mystic);
+  top: 0;
+}
+</style>
