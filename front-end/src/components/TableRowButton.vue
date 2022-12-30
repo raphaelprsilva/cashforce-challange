b6d07a7b84da04fdf38c2fd8448717f60e044fe7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-30 14:38:48 -0300

    feat(front-end): implements `TableRowButton` component

---
+++ front-end/src/components/TableRowButton.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: "TableRowButton",
+};
+</script>
+
+<template>
+  <div class="botoes-principais">
+    <div
+      class="dados-do-cedente valign-text-middle dmsans-bold-slate-gray-12px"
+    >
+      Dados do cedente
+    </div>
+  </div>
+</template>
+
+<style>
+.botoes-principais {
+  align-items: center;
+  border: 1px solid;
+  border-color: var(--periwinkle);
+  border-radius: 24px;
+  display: flex;
+  margin-left: 97px;
+  padding: 8px 29px;
+  width: fit-content;
+  cursor: pointer;
+}
+
+.dados-do-cedente {
+  letter-spacing: 0;
+  line-height: normal;
+  margin-top: -1px;
+  width: fit-content;
+}
+
+@media screen and (max-width: 1794px) {
+  .botoes-principais {
+    margin-left: 59px;
+  }
+}
+</style>
