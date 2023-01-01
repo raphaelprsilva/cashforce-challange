36ee56f46f7e919ed3cc74305a672692f25b802f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-01 17:01:07 -0300

    feat: add cors to app

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const orderRoutes = require('./routes/order.routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/orders', orderRoutes);
 
