1f63915effe0a66d3c70b6dbdf65f178fd6f2173
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-02 11:38:46 -0300

    build: set environment variables for deploy

--- back-end/src/config/config.js
+++ back-end/src/config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
   dialect: 'mysql',
 };
 
