2b7ada2e48671656bf2d45f9051262a83521f583
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2023-01-02 14:33:17 -0300

    fix(front-end): change images path

--- front-end/src/components/SideBar.vue
+++ front-end/src/components/SideBar.vue
@@ -14,14 +14,14 @@ export default {
 };
 
 const menuItemData = {
-  frame679: "./src/assets/images/frame-679@1x.png",
+  frame679: "/assets/images/frame-679@1x.png",
   propostas: "Notas fiscais",
 };
 </script>
 
 <template>
   <aside class="aside-container">
-    <MenuLogo logo="../src/assets/images/logo@1x.png" />
+    <MenuLogo logo="/assets/images/logo@1x.png" />
     <MenuItemSelected
       :frame679="menuItemData.frame679"
       :propostas="menuItemData.propostas"
